1f444b94e51abfb20e1ee1e9053bf92194b967cb
Sergio Jimenez <sergiojj932@gmail.com>
2017-09-17 13:43:25 +0200

    Includes for linux

--- source/editor/stdafx.h
+++ source/editor/stdafx.h
@@ -6,55 +6,54 @@
 #pragma once
 
 #if LINUX
-	
+    //stl includes
 	#include <functional>
-	
-	
+    #include <string>
+
+    //cocos2d-x includes
 	#include "cocos2d.h"
 
-	#include <wx/aui/dockart.h>
-	#include <wx/aui/framemanager.h>
-	#include <wx/window.h>
-	#include <wx/aui/aui.h>
-	#include <wx/grid.h>
-	#include <wx/treectrl.h>
-	#include <wx/stdpaths.h>
-	#include <wx/artprov.h>
-	#include <wx/xrc/xmlres.h>
-	#include <wx/panel.h>
-	#include <wx/gdicmn.h>
-	#include <wx/font.h>
-	#include <wx/colour.h>
-	#include <wx/settings.h>
-	#include <wx/string.h>
-	#include <wx/sizer.h>
-	#include <wx/scrolwin.h>
-	#include <wx/splitter.h>
-	#include <wx/bitmap.h>
-	#include <wx/image.h>
-	#include <wx/icon.h>
-	#include <wx/menu.h>
-	#include <wx/aui/auibook.h>
-	#include <wx/frame.h>
-	#include <wx/wx.h>
-	#include <wx/glcanvas.h>
-	#include <wx/timer.h>
-	#include <wx/dcmemory.h>
-	#include <wx/dcclient.h>
-	#include <wx/statusbr.h>
-	
+    //wxwidgets includes
+    #include <wx/aui/tabart.h>
+    #include <wx/aui/auibook.h>
+    #include <wx/aui/dockart.h>
+    #include <wx/aui/framemanager.h>
+    #include <wx/window.h>
+    #include <wx/aui/aui.h>
+    #include <wx/grid.h>
+    #include <wx/treectrl.h>
+    #include <wx/stdpaths.h>
+    #include <wx/artprov.h>
+    #include <wx/xrc/xmlres.h>
+    #include <wx/panel.h>
+    #include <wx/gdicmn.h>
+    #include <wx/font.h>
+    #include <wx/colour.h>
+    #include <wx/settings.h>
+    #include <wx/string.h>
+    #include <wx/sizer.h>
+    #include <wx/scrolwin.h>
+    #include <wx/splitter.h>
+    #include <wx/bitmap.h>
+    #include <wx/image.h>
+    #include <wx/icon.h>
+    #include <wx/menu.h>
+    #include <wx/aui/auibook.h>
+    #include <wx/frame.h>
+    #include <wx/wx.h>
+    #include <wx/glcanvas.h>
+    #include <wx/timer.h>
+    #include <wx/dcmemory.h>
+    #include <wx/dcclient.h>
+    #include <wx/statusbr.h>
 #endif
 
 #if WIN32
 	#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 	
-
 	//stl includes
 	#include <functional>
-	#include <string>       // std::string
-	//#include <iostream>     // std::cout
-	//#include <sstream>      // std::stringstream
-
+    #include <string>
 	
 	//cocos2d-x includes
 	#define ssize_t SSIZE_T
@@ -62,12 +61,10 @@
 		#include <WS2tcpip.h>
 		#include <Windows.h>
 		#include "cocos2d.h"
-	#undef ssize_t		
-		//wxwidgets includes
+    #undef ssize_t
+
+    //wxwidgets includes
 	#define ssize_t wxInt32
-
-
-
 		#include <wx/aui/tabart.h>
 		#include <wx/aui/auibook.h>
 		#include <wx/aui/dockart.h>
